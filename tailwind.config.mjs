d5e484e8a5e0ac0a39fb79fcfb355cc7f4b981c0
Maribel Ferreira <maribelferreira@users.noreply.github.com>
2025-05-07 19:36:38 +0100

    Update dependencies and enhance styling: Upgraded Astro and Tailwind packages, added gradient animation to Tailwind config, and improved footer and hero components with new designs and animations. Updated project links and refined layout for better user experience.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			animation: {
+				gradient: 'gradient 3s linear infinite',
+			},
+			keyframes: {
+				gradient: {
+					'0%, 100%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' }
+				},
+			},
 		},
 	},
 	plugins: [],
